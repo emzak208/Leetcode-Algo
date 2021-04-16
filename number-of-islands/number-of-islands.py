63da2497226f67b4cb129fdcd6a210793887aeb4
mihap <emzak208@users.noreply.github.com>
2021-04-16 00:23:46 -0700

    Time: 200 ms (6.09%), Space: 19.5 MB (8.34%) - LeetHub

---
+++ number-of-islands/number-of-islands.py
@@ -0,0 +1,45 @@
+from collections import deque 
+
+directions = [(0,1),(-1,0),(0,-1),(1,0)] # up, left, down, right 
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        for g in grid:
+            print(g)
+        if not grid or not grid[0]:
+            return 0
+        n_island = 0
+        visited = set()
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                # print('Check ({x},{y}) with value {v}'.format(x=i, y=j, v=grid[i][j]))
+                if grid[i][j] == "1" and (i,j) not in visited: # only bfs when sees '1'
+                    self.bfs(grid, visited, i, j)
+                    n_island += 1 
+        return n_island
+        
+    def bfs(self, grid, visited, x, y):
+        queue = deque([(x,y)])
+        visited.add((x,y))
+        while queue:
+            x, y = queue.popleft()
+            visited.add((x, y))
+            print('add ({x},{y}) to visited'.format(x=x, y=y))
+            for delta_x, delta_y in directions:
+                next_x = x + delta_x
+                next_y = y + delta_y
+                if not self.is_valid(grid, next_x, next_y, visited):
+                    continue 
+                queue.append((next_x,next_y))
+                visited.add((next_x,next_y))
+       
+    def is_valid(self, grid, x, y, visited):
+        m, n = len(grid), len(grid[0])
+        if x < 0 or x >= m or y < 0 or y >= n:
+            return False
+        if (x, y) in visited:
+            return False
+        return grid[x][y] == "1" # pay attention, some questions values in grid are numeric
+    
+
+        
