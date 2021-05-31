4e2d6bd7927ca6ba56174eaef5267bd55935d35b
mihap <emzak208@users.noreply.github.com>
2021-05-31 00:18:12 -0700

    Time: 444 ms (5.19%), Space: 15.3 MB (85.69%) - LeetHub

--- graph-valid-tree/graph-valid-tree.py
+++ graph-valid-tree/graph-valid-tree.py
@@ -1,11 +1,19 @@
+# 思路： 
+# 1. 一棵拥有n个节点的树有n-1条边，树是连通的，没有环的。
+# 2. 给定一个无向图让我们判断是否为树，我们只需要判断是否连通且无环即可。
+# 3. 我们可以从根节点出发向儿子节点进行广度优先搜索bfs，如果能遍历完所有的点，且没有环存在，那么说明这个无向图是树。
+# 4. 已知给定的边不重复，所以可以通过判断边数是否为(n - 1)条来判断是否无环。
+
+
 from collections import deque 
 
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        print(len(edges))
-        if n == 0 or len(edges) != n - 1:
+        if n == 0 or len(edges) != n - 1: # edges为空是TRUE，因此不能加在这里
             return False 
         
+        # new_edges里存edges里所有的[start,end],以及他们的reverse[end,start]
+        # 如果只存正序没有倒序，当给的edges是倒序时，就不满足start = node， bfs就会跳过造成错误结果
         new_edges = []
         for start, end in edges:
             new_edges.append([start,end])
@@ -20,25 +28,10 @@ class Solution:
                 if start == node and end not in visited:
                     queue.append(end)
                     visited.add(end)
+         
+        # 最后判断是否有环
         if len(visited) == n:
             return True 
-        return False 
+        return False
             
         
-#     new_edges = []
-#     for [a, b] in edges:
-#       new_edges.append([a, b])
-#       new_edges.append([b, a])
-            
-#     queue = deque([0])
-#     visited = set([0])
-#     while queue:
-#       node = queue.popleft()
-#       for [a, b] in new_edges:
-#         if a == node and b not in visited:
-#           visited.add(b)
-#           queue.append(b)
-    
-#     if len(visited) == n:
-#       return True
-#     return False
