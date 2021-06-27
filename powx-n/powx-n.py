f32ba139246593b96b02df81fed4655a8005e609
mihap <emzak208@users.noreply.github.com>
2021-06-27 12:24:06 -0700

    Time: 32 ms (60.53%), Space: 14.2 MB (48.41%) - LeetHub

---
+++ powx-n/powx-n.py
@@ -0,0 +1,17 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n == 0:
+            return 1
+        if n < 0:
+            x = 1/x
+            n = -n
+          
+        # if n is odd: x*x^(2*(n-1)/2) = x*x^(n-1)
+        if n % 2 == 1: 
+            return x*self.myPow(x**2, (n-1)/2)
+        else: 
+            return self.myPow(x**2, n/2)
+        
+        
+            
+        
