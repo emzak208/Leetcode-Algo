e7898838f1e46ab97c5239c2c8f2cb118a16dc48
mihap <emzak208@users.noreply.github.com>
2022-02-01 08:13:04 -0800

    Time: 630 ms (30.61%), Space: 0B (100.00%) - LeetHub

--- 550-game-play-analysis-iv/550-game-play-analysis-iv.sql
+++ 550-game-play-analysis-iv/550-game-play-analysis-iv.sql
@@ -1,5 +1,6 @@
 # Write your MySQL query statement below
 
+## method 1
 # select round(count(distinct player_id)/(select count(distinct player_id) from Activity),2) fraction
 # from 
 # (select player_id, event_date,
@@ -7,6 +8,7 @@
 # from Activity) a 
 # where (event_date - first_date) = 1 
 
+## method 2
 select 
 round(count(distinct(case when event_date - first_date = 1 then player_id else null end))/count(distinct player_id),2) fraction
 from 
