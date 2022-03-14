535bf8c6724b7417be80159d172f4dbfb6cf896e
mihap <emzak208@users.noreply.github.com>
2022-03-14 15:04:17 -0700

    Time: 1196 ms (5.01%), Space: 0B (100.00%) - LeetHub

--- 184-department-highest-salary/184-department-highest-salary.sql
+++ 184-department-highest-salary/184-department-highest-salary.sql
@@ -9,6 +9,7 @@
 # on e.DepartmentId = d.Id) t1 
 # where rk = 1 
 
+## -------------
 ## select employee with max salary 
 # select Name
 # from Employee 
@@ -23,11 +24,14 @@
 # (select DepartmentId, sum(Salary) total_salary from Employee
 # group by DepartmentId) a ) b)
 
-select t.Department,t.Employee,t.Salary
-from 
-(select d.id, d.name Department, e.name Employee, e.Salary, 
-dense_rank() over (partition by d.id order by e.salary desc) rk 
-from Employee e 
-join Department d
-on e.departmentId = d.id) t
-where rk = 1
+select Department, Employee, Salary
+from (
+select d.name as Department, e.name as Employee, 
+e.salary as Salary,
+dense_rank() over(partition by d.id order by e.salary desc) rnk
+from Department d 
+join Employee e 
+on d.id = e.departmentId
+) tmp
+where rnk = 1
+
