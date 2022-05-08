7300c6a97beaa0e4eb14f15ef40a0d5544d55cb8
mihap <emzak208@users.noreply.github.com>
2022-05-08 01:14:54 -0700

    Time: 551 ms (75.80%), Space: 0B (100.00%) - LeetHub

--- 184-department-highest-salary/184-department-highest-salary.sql
+++ 184-department-highest-salary/184-department-highest-salary.sql
@@ -1,15 +1,15 @@
 # Write your MySQL query statement below
 
-# select Department,Employee,Salary
-# from 
-# (select d.Id, d.Name as Department, e.Name as Employee, e.Salary,
-#  dense_rank() over (partition by d.Id order by e.Salary desc) rk
-# from Employee e 
-# join Department d 
-# on e.DepartmentId = d.Id) t1 
-# where rk = 1 
+select Department,Employee,Salary
+from 
+(select d.Id, d.Name as Department, e.Name as Employee, e.Salary,
+ dense_rank() over (partition by d.Id order by e.Salary desc) rk
+from Employee e 
+join Department d 
+on e.DepartmentId = d.Id) t1 
+where rk = 1 
 
-## -------------
+
 ## select employee with max salary 
 # select Name
 # from Employee 
@@ -24,14 +24,19 @@
 # (select DepartmentId, sum(Salary) total_salary from Employee
 # group by DepartmentId) a ) b)
 
-select Department, Employee, Salary
-from (
-select d.name as Department, e.name as Employee, 
-e.salary as Salary,
-dense_rank() over(partition by d.id order by e.salary desc) rnk
-from Department d 
-join Employee e 
-on d.id = e.departmentId
-) tmp
-where rnk = 1
+# # select all employee in the department with the highest salasy 
+# select name
+# from Employee 
+# where departmentId in (select departmentId from
+# (select Department, departmentId,
+# dense_rank() over (order by Salary desc) rk
+# from
+# (select t1.name Employee, t2.id as departmentId, t2.name as Department,
+#  sum(salary) Salary 
+#  from Employee t1 
+#  left join Department t2 
+#  on t1.departmentid = t2.id 
+# group by t2.id,t2.name) a 
+# ) b 
+# where rk =1) 
 
