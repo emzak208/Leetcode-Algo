2a0dd92fdd33e9fa6d44f2af4cf7b6067f71beb0
mihap <emzak208@users.noreply.github.com>
2022-06-01 21:57:08 -0700

    Time: 281 ms (76.91%), Space: 0B (100.00%) - LeetHub

--- 570-managers-with-at-least-5-direct-reports/570-managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/570-managers-with-at-least-5-direct-reports.sql
@@ -6,10 +6,3 @@ left join Employee m
 on e.managerId = m.id 
 group by e.managerId 
 having count(m.id) >=5 
-
-# select * # distinct m.name 
-# from Employee e 
-# left join Employee m 
-# on e.managerId = m.id
-# group by m.id
-# having count(e.id) >=5
