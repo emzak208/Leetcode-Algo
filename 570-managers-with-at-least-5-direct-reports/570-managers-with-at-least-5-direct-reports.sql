09dd8b4e699b8da787233ede44aebfb04aac0f9e
mihap <emzak208@users.noreply.github.com>
2022-06-01 21:59:37 -0700

    Time: 284 ms (75.27%), Space: 0B (100.00%) - LeetHub

--- 570-managers-with-at-least-5-direct-reports/570-managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/570-managers-with-at-least-5-direct-reports.sql
@@ -1,8 +1,25 @@
 # Write your MySQL query statement below
 
-select distinct m.name
-from Employee e
-left join Employee m
-on e.managerId = m.id 
-group by e.managerId 
-having count(m.id) >=5 
+# select distinct m.name
+# from Employee e
+# left join Employee m
+# on e.managerId = m.id 
+# group by e.managerId 
+# having count(m.id) >=5 
+# ;
+
+## or 
+# select m.name 
+# from Employee e 
+# left join Employee m 
+# on e.managerId = m.id 
+# group by m.name
+# having count(*) >= 5
+# ;
+
+select e2.Name
+from employee e1
+join employee e2
+on e1.ManagerId=e2.Id
+group by e2.name
+having count(*)>=5
