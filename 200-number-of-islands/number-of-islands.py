54f27eed73e8cd11ef2100fc89f172d530ea3692
mihap <emzak208@users.noreply.github.com>
2023-11-17 21:48:32 -0800

    Time: 341 ms (24.32%) | Memory: 24.3 MB (24.17%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,129 +1,42 @@
 from collections import deque 
-directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+directions = [(0,1),(-1,0),(0,-1),(1,0)] # up, left, down, right 
 
 class Solution:
-    def numIslands(self, grid: List[List[str]]) -> int:
-        visitted = set()
-        num_islands = 0 
 
+    def numIslands(self, grid: List[List[str]]) -> int:
         if not grid or not grid[0]:
             return 0
-
+        n_island = 0
+        visited = set()
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] == '1' and (i,j) not in visitted:
-                    self.bfs(grid, visitted, i, j)
-                    num_islands += 1 
-        return num_islands
-
-    def bfs(self, grid, visitted, x, y):
-        queue = deque([(x, y)])
-        visitted.add((x, y))
-
+                if grid[i][j] == "1" and (i,j) not in visited: # only bfs when sees '1'
+                    self.bfs(grid, visited, i, j)
+                    n_island += 1 
+        return n_island
+        
+    def bfs(self, grid, visited, x, y):
+        queue = deque([(x,y)])
+        visited.add((x,y))
+        
         while queue:
-            (x, y) = queue.popleft()
-            visitted.add((x, y))
-            for (delta_x, delta_y) in directions: 
-                next_x = x + delta_x 
+            x, y = queue.popleft()
+            visited.add((x, y))
+            for delta_x, delta_y in directions:
+                next_x = x + delta_x
                 next_y = y + delta_y
-                if not self.isValid(grid, visitted, next_x, next_y):
-                    continue
-                queue.append((next_x, next_y))
-                visitted.add((next_x, next_y))
-
-    def isValid(self, grid, visitted, x, y):
-        m, n = len(grid), len(grid[0])
-
-        if x < 0 or x >= m or y < 0 or y >= n :
-            return False 
-        if (x, y) in visitted:
-            return False 
-        return grid[x][y] == '1'
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# directions = [(0,1),(-1,0),(0,-1),(1,0)] # up, left, down, right 
-
-    # def numIslands(self, grid: List[List[str]]) -> int:
-    #     for g in grid:
-    #         print(g)
-    #     if not grid or not grid[0]:
-    #         return 0
-    #     n_island = 0
-    #     visited = set()
-    #     for i in range(len(grid)):
-    #         for j in range(len(grid[0])):
-    #             # print('Check ({x},{y}) with value {v}'.format(x=i, y=j, v=grid[i][j]))
-    #             if grid[i][j] == "1" and (i,j) not in visited: # only bfs when sees '1'
-    #                 self.bfs(grid, visited, i, j)
-    #                 n_island += 1 
-    #     return n_island
-        
-    # def bfs(self, grid, visited, x, y):
-    #     queue = deque([(x,y)])
-    #     visited.add((x,y))
-        
-    #     while queue:
-    #         x, y = queue.popleft()
-    #         visited.add((x, y))
-    #         # print('add ({x},{y}) to visited'.format(x=x, y=y))
-    #         for delta_x, delta_y in directions:
-    #             next_x = x + delta_x
-    #             next_y = y + delta_y
-    #             if not self.is_valid(grid, next_x, next_y, visited):
-    #                 continue 
-    #             queue.append((next_x,next_y))
-    #             visited.add((next_x,next_y))
+                if not self.is_valid(grid, next_x, next_y, visited):
+                    continue 
+                queue.append((next_x,next_y))
+                visited.add((next_x,next_y))
        
-    # def is_valid(self, grid, x, y, visited):
-    #     m, n = len(grid), len(grid[0])
-    #     if x < 0 or x >= m or y < 0 or y >= n:
-    #         return False
-    #     if (x, y) in visited:
-    #         return False
-    #     return grid[x][y] == "1" # pay attention, some questions values in grid are numeric
+    def is_valid(self, grid, x, y, visited):
+        m, n = len(grid), len(grid[0])
+        if x < 0 or x >= m or y < 0 or y >= n:
+            return False
+        if (x, y) in visited:
+            return False
+        return grid[x][y] == "1" # pay attention, some questions values in grid are numeric
     
 
         
