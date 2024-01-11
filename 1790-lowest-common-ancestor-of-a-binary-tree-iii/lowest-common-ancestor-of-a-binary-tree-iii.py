edf9f05339f88b9428d0c436e5befb3928a8675a
mihap <emzak208@users.noreply.github.com>
2024-01-10 22:34:55 -0800

    Time: 41 ms (98.97%) | Memory: 20.6 MB (88.64%) - LeetSync

--- 1790-lowest-common-ancestor-of-a-binary-tree-iii/lowest-common-ancestor-of-a-binary-tree-iii.py
+++ 1790-lowest-common-ancestor-of-a-binary-tree-iii/lowest-common-ancestor-of-a-binary-tree-iii.py
@@ -11,17 +11,47 @@ class Node:
 class Solution:
     def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
         # Method 1 : naive approqach 
-        seen = set()
+        # seen = set()
 
-        while p: 
-            seen.add(p)
+        # while p: 
+        #     seen.add(p)
 
-            p = p.parent 
+        #     p = p.parent 
 
-        while q: 
-            if q in seen:
-                return q
-                
-            q = q.parent
+        # while q: 
+        #     if q in seen:
+        #         return q
+
+        #     q = q.parent
+
+        # Method 2: brillaint solution, need to memorise 
+        p_copy = p
+        q_copy = q
+        
+        if p_copy == q_copy:
+            return p_copy
+
+        while p_copy != q_copy:
+            p_copy = p_copy.parent
+            q_copy = q_copy.parent
+
+            if p_copy is None:
+                p_copy = q
+            if q_copy is None:
+                q_copy = p
+
+        return p_copy
+
+        
+
+
+
+
+
+
+
+
+
+
 
 
