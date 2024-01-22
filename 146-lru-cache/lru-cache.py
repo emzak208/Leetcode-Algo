dffee8b384f50bbe405338c8c075ae6bb8944eb6
mihap <emzak208@users.noreply.github.com>
2024-01-21 22:02:31 -0800

    Time: 533 ms (95.25%) | Memory: 77.2 MB (95.54%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,4 +1,4 @@
-from collections import deque
+
 class LRUCache:
 
     def __init__(self, capacity: int):
@@ -7,11 +7,17 @@ class LRUCache:
 
     def get(self, key: int) -> int:
         if key not in self.dict:
-            return -1
-
+            return -1 
+        
         self.dict[key] = self.dict.pop(key)
-
         return self.dict[key]
+
+        # if key not in self.dict:
+        #     return -1
+
+        # self.dict[key] = self.dict.pop(key) # remove from list and add to the end of dict
+
+        # return self.dict[key]
         
     def put(self, key: int, value: int) -> None:
         if key in self.dict:
@@ -21,8 +27,25 @@ class LRUCache:
                 self.freeSpace -= 1
             else:
                 self.dict.pop(next(iter(self.dict)))
-
+        
         self.dict[key] = value
+
+
+
+
+
+
+
+
+        # if key in self.dict:
+        #     self.dict.pop(key)
+        # else:
+        #     if self.freeSpace:
+        #         self.freeSpace -= 1
+        #     else:
+        #         self.dict.pop(next(iter(self.dict)))
+
+        # self.dict[key] = value
 
 
         
